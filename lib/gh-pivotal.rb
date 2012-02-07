f2a4fe88ed82ab781a73a18f7a0ef898153274cc
Mariano A. Cortesi <mariano@zauberlabs.com>
2012-02-07 17:10:43 -0300

    centralize configuration

--- lib/gh-pivotal.rb
+++ lib/gh-pivotal.rb
@@ -1,11 +1,19 @@
 require 'octokit'
 require 'sinatra'
 
-use Rack::Auth::Basic, "Restricted Area" do |username, password|
-  [username, password] == ['admin', 'zauberlabs']
+configure do
+  set :gh_user, "mcortesi"
+  set :gh_password, "password here"
+  set :basic_user, 'admin'
+  set :basic_password, 'zauberlabs'
 end
 
-$ghcli = Octokit::Client.new :login => "mcortesi" , :password => "password!"
+
+use Rack::Auth::Basic, "Restricted Area" do |username, password|
+  [username, password] == [settings.basic_user, settings.basic_password]
+end
+
+$ghcli = Octokit::Client.new :login => settings.gh_user , :password => settings.gh_password
 
 helpers do
   def fetch_issues(reponame)
@@ -25,4 +33,3 @@ get '/issues/*' do |reponame|
   @issues = fetch_issues reponame
   nokogiri :issues
 end
-
